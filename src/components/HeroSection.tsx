b9bede6459c2d643da24bda980310ff20e777246
jaymehta002 <jaymehta002@gmail.com>
2024-03-16 18:50:49 +0530

    feature

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,39 +1,102 @@
+import Image from "next/image";
+
 export default function HeroSection() {
-    return (
-        <div className="relative font-inter h-screen overflow-hidden bg-[url('/background.svg')] bg-top top-0 bg-white bg-no-repeat ">
+  return (
+    <>
+    <section id="hero">
+    <div className="relative font-inter h-screen overflow-hidden bg-[url('/img/background.svg')] bg-top top-0 bg-white bg-no-repeat ">
         <div className="max-w-[85rem]  mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
-            <div className="flex justify-center">
-            </div>
+        <div className="flex justify-center"></div>
 
-            <div className="mt-16 max-w-xl text-center mx-auto">
+        <div className="mt-16 max-w-xl text-center mx-auto">
             <h1 className="block font-bold text-gray-800 text-5xl md:text-5xl lg:text-8xl">
-                Every Pixel Matters
+            Every Pixel Matters
             </h1>
-            </div>
+        </div>
 
-            <div className="mt-5 max-w-3xl text-center mx-auto">
-            <p className="md:text-xl text-base text-gray-600">Discover Pixel Perfect, the ultimate open-source companion, empowering you to achieve flawless web design mastery, down to the last pixel.</p>
+        <div className="mt-5 max-w-3xl text-center mx-auto">
+            <p className="md:text-xl text-base text-gray-600">
+            Discover Pixel Perfect, the ultimate open-source companion,
+            empowering you to achieve flawless web design mastery, down to
+            the last pixel.
+            </p>
+        </div>
+
+        <form className="flex mt-8 items-center max-w-sm mx-auto">
+            <label htmlFor="simple-search" className="sr-only">
+            Search
+            </label>
+            <div className="relative w-full">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 20"
+                >
+                <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"
+                />
+                </svg>
             </div>
-            
-            
-            <form className="flex mt-8 items-center max-w-sm mx-auto">   
-                <label htmlFor="simple-search" className="sr-only">Search</label>
-                <div className="relative w-full">
-                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"/>
-                        </svg>
-                    </div>
-                    <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full ps-10 p-2.5  " placeholder="Enter your url..." required />
-                </div>
-                <button type="submit" className="p-2.5 ms-2 text-sm font-medium text-white bg-purple-700 rounded-lg border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 ">
-                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                    </svg>
-                    <span className="sr-only">Search</span>
-                </button>
-            </form>
+            <input
+                type="text"
+                id="simple-search"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full ps-10 p-2.5  "
+                placeholder="Enter your url..."
+                required
+            />
+            </div>
+            <button
+            type="submit"
+            className="p-2.5 ms-2 text-sm font-medium text-white bg-purple-700 rounded-lg border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 "
+            >
+            <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+            >
+                <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+            </svg>
+            <span className="sr-only">Search</span>
+            </button>
+        </form>
         </div>
+    </div>
+    </section>
+    <div className="container mx-auto">
+      <section id='features' className="bg-white flex flex-wrap justify-between items-center">
+        <div className="w-full md:w-1/2 px-4">
+          <Image src='/img/screen5.png' alt=" img" width={300} height={200} className="w-full" />
         </div>
-    )
+        <div className="w-full md:w-1/2 px-4">
+          {/* Add your text content here */}
+          <h2 className="text-6xl font-bold mb-4">Responsive Design, Limitless Possibilities.</h2>
+          <div className="font-inter text-gray-800">
+          <ul style={{ listStyleType: 'disc' }}>
+            <li><b>Seamless adaptation</b>: Websites dynamically adjust layout and content for any screen size or device.</li>
+            <li><b>Enhanced user experience</b>: Consistent and intuitive browsing experiences across desktops, tablets, and smartphones.</li>
+            <li><b>Versatile solutions</b>: Craft immersive digital journeys that captivate and retain users.</li>
+            <li><b>Future-proofing</b>: Stay ahead in the ever-evolving digital landscape with adaptive design principles.</li>
+        </ul>
+
+          </div>
+        </div>
+      </section>
+    </div>
+    </>
+  );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,7 +15,7 @@ export default function NavBar() {
       <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
         <a href="#" className="flex items-center">
           <Image
-            src="/logo.svg"
+            src="/img/logo.svg"
             alt="Landwind Logo"
             width={24}
             height={24}
@@ -27,7 +27,7 @@ export default function NavBar() {
             <span></span>
           </div>
           <a
-            href="https://themesberg.com/product/tailwind-css/landing-page"
+            href="#"
             className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
           >
             Download
